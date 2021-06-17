d384e2a134665bd82187faaca7710460052185d8
manjot29singh <85251332+manjot29singh@users.noreply.github.com>
2021-06-17 13:40:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+function clicked()
+	{
+	alert("Get 20% discount on ABC debit cards ")
+	}
+	function clicked1()
+	{
+		alert("Plate prices starting from 850 RS/Plate")
+	}
+	function clicked2()
+	{
+	alert("Buy 2 get 1 Free")
+	
+	}
+	function clicked3()
+	{
+	alert("Get Free Home Delivery free upto 10kms on order of min 500RS")
+	
+	}
+	function calculateTip() {
+		var billAmt = document.getElementById("billamt").value;
+		var serviceQual = document.getElementById("serviceQual").value;
+		var numOfPeople = document.getElementById("peopleamt").value;
+		if (billAmt == "" || serviceQual == 0) {
+		  alert("Please enter values");
+		  return;
+		}
+		if (numOfPeople === "" || numOfPeople <= 1) {
+		  numOfPeople = 1;
+		  document.getElementById("each").style.display = "none";
+		} else {
+		  document.getElementById("each").style.display = "block";
+		}
+
+		var total = (billAmt * serviceQual) / numOfPeople;
+		total = Math.round(total * 100) / 100;
+		total = total.toFixed(2);
+		document.getElementById("totalTip").style.display = "block";
+		document.getElementById("tip").innerHTML = total;
+	  
+	  }
+	  document.getElementById("totalTip").style.display = "none";
+	  document.getElementById("each").style.display = "none";
+	  document.getElementById("calculate").onclick = function() {
+		calculateTip()
+	  
+	  };
+	 
